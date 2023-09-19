e43dbf27858fdd5b47c03a1ff5e6dee23a7c805f
Nat Tuck <nat@ferrus.net>
2023-09-19 08:08:21 -0400

    Deploy script.

--- app/page.js
+++ app/page.js
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <main className="text-center min-h-screen">
-      <h1 className="text-2xl py-8">Today's Joke</h1>
+      <h1 className="text-2xl py-8">Today&apos;s Joke</h1>
       <p>Joke of the Day goes here</p>
     </main>
   )
