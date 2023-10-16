81788820b8796d885beb87c0542b2a6aa211762e
Nat Tuck <nat@ferrus.net>
2023-10-16 08:25:43 -0400

    Add coverage

--- jest.config.mjs
+++ jest.config.mjs
@@ -13,6 +13,9 @@ const config = {
   // setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
  
   testEnvironment: 'jest-environment-jsdom',
+  collectCoverageFrom: [
+    'app/**/*.{js,jsx}'
+  ]
 };
  
 // createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
